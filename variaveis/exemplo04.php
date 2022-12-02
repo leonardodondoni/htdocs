c60f617a75d6a6ccc9f9fd5cb148d336e2101f41
Leonardo <leonardosiqueira387@gmail.com>
2022-12-01 23:29:41 -0300

    Ip address, script name etc.

--- variaveis/exemplo04.php
+++ variaveis/exemplo04.php
@@ -1,3 +1,18 @@
 <?php
 
+//a variavel nome recebera todas as variaves recebidas atraves da URL, metodo GET.
+//$GET sempre vai ser String
+//para castar a varíavel (int)
+
+//$nome = $_GET["a"];
+$nome = (int)$_GET["a"];
+
+//var_dump($nome);
+
+// para obter o IP do cliente $ip = $_SERVER["REMOTE_ADDR"];
+
+$ip = $_SERVER["SCRIPT_NAME"];
+
+echo $ip;
+
 ?>
